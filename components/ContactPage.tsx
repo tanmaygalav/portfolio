1a058275aa19802f30458ecf44ec20c9c2e2e72c
Tanmay Galav <tanmaygalav@gmail.com>
2025-08-15 18:26:41 +0530

    Update ContactPage.tsx

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -15,22 +15,22 @@ const ContactPage: React.FC = () => {
           I'm currently open to new opportunities and collaborations. Whether you have a question or just want to say hi, feel free to reach out. I'll do my best to get back to you!
         </p>
         <a 
-          href={`mailto:${SOCIAL_LINKS.email}`} 
+          href={`mailto:${tanmaygalav@gmail.com}`} 
           className="inline-flex items-center gap-2 bg-white text-black font-semibold py-3 px-6 rounded-lg hover:bg-gray-200 transition-colors duration-300"
         >
           <MailIcon className="w-5 h-5" />
           Say Hello
         </a>
         <div className="mt-12 flex justify-center space-x-6">
-          <a href={SOCIAL_LINKS.github} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
+          <a href={github.com/Vitiantanmay} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
             <GithubIcon className="w-8 h-8" />
           </a>
-          <a href={SOCIAL_LINKS.linkedin} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
+          <a href={linkedin.com/in/vitiantanmay/} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
             <LinkedinIcon className="w-8 h-8" />
           </a>
-          <a href={SOCIAL_LINKS.x} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
+{/*           <a href={SOCIAL_LINKS.x} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
             <XIcon className="w-8 h-8" />
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
