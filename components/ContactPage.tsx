b2d91f7c49c6f92aa9400009f1766602787d8279
Tanmay Galav <tanmaygalav@gmail.com>
2025-08-15 18:35:06 +0530

    Update ContactPage.tsx

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { SOCIAL_LINKS } from '../constants';
 import GithubIcon from './icons/GithubIcon';
@@ -15,22 +14,24 @@ const ContactPage: React.FC = () => {
           I'm currently open to new opportunities and collaborations. Whether you have a question or just want to say hi, feel free to reach out. I'll do my best to get back to you!
         </p>
         <a 
-          href={`mailto:${"tanmaygalav@gmail.com"}`} 
+          href={`mailto:${SOCIAL_LINKS.email}`} 
           className="inline-flex items-center gap-2 bg-white text-black font-semibold py-3 px-6 rounded-lg hover:bg-gray-200 transition-colors duration-300"
         >
           <MailIcon className="w-5 h-5" />
           Say Hello
         </a>
         <div className="mt-12 flex justify-center space-x-6">
-          <a href={"github.com/Vitiantanmay"} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
+          <a href={SOCIAL_LINKS.github} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
             <GithubIcon className="w-8 h-8" />
           </a>
-          <a href={"linkedin.com/in/vitiantanmay/"} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
+          <a href={SOCIAL_LINKS.linkedin} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
             <LinkedinIcon className="w-8 h-8" />
           </a>
-{/*           <a href={SOCIAL_LINKS.x} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
-            <XIcon className="w-8 h-8" />
-          </a> */}
+          {SOCIAL_LINKS.x && (
+            <a href={SOCIAL_LINKS.x} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
+              <XIcon className="w-8 h-8" />
+            </a>
+          )}
         </div>
       </div>
     </div>
