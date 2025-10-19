d5bf9a4b35209325956bdf77f3fbfa0cc2c6a55a
Tanmay Galav <tanmaygalav@gmail.com>
2025-10-19 13:30:34 +0530

    Mt added

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -4,7 +4,7 @@ import { SKILLS } from '../constants';
 
 const AboutPage: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center mt-20">
       <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12">
         <div className="flex flex-col justify-center">
           <h2 className="text-4xl font-bold mb-4 tracking-tighter">About Me</h2>
